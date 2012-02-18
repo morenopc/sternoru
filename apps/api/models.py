28c3223f0f52f6b99d7c9490195adde01397471c
Moreno <moreno.pinheiro@gmail.com>
2012-02-18 14:48:28 -0200

    api urls.py and admin.py done

---
+++ apps/api/models.py
@@ -0,0 +1,21 @@
+# -*- coding: UTF8 -*-
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
+
+class Profile(models.Model):
+    user=models.ForeignKey(User,unique=True,verbose_name=_('user'))
+    sex=models.PositiveSmallIntegerField(_('gender'),choices=((1,'M'),(0,'F')),default=1)
+
+class Place(models.Model):
+    name=models.CharField(_('name'), max_length=64)
+    lat=models.FloatField(_('latitude'))
+    lng=models.FloatField(_('longitude'))
+    address=models.CharField(_('address'), max_length=64)
+
+class Checkin(models.Model):
+    profile=models.ForeignKey(Profile,unique=True,verbose_name=_('user profile'))
+    place=models.ForeignKey(Place,unique=True,verbose_name=_('place'))
+    time=models.DateTimeField(_('alterado em'), auto_now=True)
+
